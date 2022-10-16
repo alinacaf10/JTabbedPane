545017658b91a4f2a6c9773447ae5db2496fc866
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-16 12:06:52 +0400

    JTabbed pane added

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,32 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame();
+        JLabel lb1 = new JLabel("First pane");
+        JPanel p1 = new JPanel();
+        p1.add(lb1);
+
+        JLabel lb2 = new JLabel("Second pane");
+        JPanel p2 = new JPanel();
+        p2.add(lb2);
+
+        JLabel lb3 = new JLabel("Third pane");
+        JPanel p3 = new JPanel();
+        p3.add(lb3);
+
+        JTabbedPane tb=new JTabbedPane();
+        tb.setBounds(50,50,200,200);
+        tb.add("First",p1);
+        tb.add("Second",p2);
+        tb.add("Third",p3);
+
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setSize(400, 400);
+        frame.add(tb);
     }
 }
